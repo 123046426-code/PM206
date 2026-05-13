7c12c787293e3a0d0677957328a62bea7667e34c
Spica55 <123046426@upq.edu.mx>
2026-05-12 19:14:02 -0600

    Servidor
    Se  añadio un archivo para trabajar por parte del servidor

--- practica1/servidor.js
+++ practica1/servidor.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo Js desde el servidor");
+console.log("Hola Mundo Js desde el servidor")
+
+/* Operaciones */
+let edad1 = 10
+let edad2 = 22
+
+console.log("Edad promedio")
+console.log((edad1 + edad2) / 2)
+
+/* Medir tiempo de un proceso */
+console.time('miproceso')
+
+    for(let i=0; i<1000000; i++){
+        let x = i*2
+    }
+
+console.timeEnd('miproceso')
+
+/* Objetos tipo tabla */
+let usuarios= [
+    {nombre: "andy", edad: 20},
+    {nombre: "miguel", edad: 22}
+]
+console.table(usuarios)
