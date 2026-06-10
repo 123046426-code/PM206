cab9043161d3188e34eb0570ee12530ca49c1d00
Spica55 <123046426@upq.edu.mx>
2026-06-09 18:51:12 -0600

    Componentes 1
    Se añadieron diferentes tipos de componentes como ejemplo para conocer la utilización de estos mismos.

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -1,15 +1,25 @@
+/*Zona1: Importaciones de componentes y archivos*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import { Saludo2 } from './components/Saludo2';
 
+//Zona2: Main - Hogar de los componentes
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/wave.png')} />
+      <Text>Hola mundo</Text>
+      <Text>-----------Componente Simple-------------</Text>
+      <Saludo />
+      <Text>-----------Componente Compuesto-------------</Text>
+      <Saludo2 />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Zona3: Estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ practica5/miapp2/components/Saludo.js
@@ -0,0 +1,9 @@
+
+import {Button} from 'react-native';
+
+export const Saludo= () => {
+    return(
+        <Button title="Hola 206"/>
+    );
+}
+
