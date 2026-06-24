af5d87c1027038f8cd41ee1599849e7fc4eb72ff
Spica55 <123046426@upq.edu.mx>
2026-06-23 19:09:25 -0600

    Pressable & Switch
    Se añadio el componente de pressable y Switch de miguelon

--- practica5/miapp2/screens/PressableScreen.js
+++ practica5/miapp2/screens/PressableScreen.js
@@ -1,28 +1,82 @@
-/*Zona1: Importaciones de componentes y archivos*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable, Switch } from 'react-native';
+import { useState } from 'react';
 
 
-//Zona2: Main - Hogar de los componentes
 export default function PressableScreen () {
+  const [buttonText, setButtonText] = useState("Dame click");
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Aqui va la práctica de Miguelito</Text>
-      <StatusBar style="auto"/>
+    <View 
+    style = {[styles.container, {backgroundColor: isDarkMode ? "black" : "white"}]}
+    >
+      <StatusBar style={isDarkMode ? "light" : "dark"}/>
+      <Pressable style={styles.button}
+      onPress={() =>{
+        console.log("Se presionó el botón");
+        setButtonText("Botón presionado")
+      }}
+
+      onPressIn={() =>{
+        console.log("Se acaba de presionar el botón");
+      }}
+
+      onPressOut={() =>{
+        console.log("Se acaba de soltar el botón");
+      }}
+
+      onLongPress={() =>{
+        console.log("Presión prolongada");
+        setButtonText("Presion prolongada detectada");
+      }}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </Pressable>
+
+      <Text style={{color: isDarkMode ? "white" : "black"}}>Modo oscuro</Text>
+
+      <Switch
       
+      value={isDarkMode}
+      onValueChange={(previousState) => setIsDarkMode((previousState) =>
+        !previousState)}
+      trackColor={{false: "#767577", true: "lightblue"}}
+      thumColor={"#f4f3f4"}
+
+      />
     </View>
   );
 }
 
 //Zona3: Estilos y posicionamiento
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  tarjetaRoja:{backgroundColor: '#FF6B6B'},
-  tarjetaVerde:{backgroundColor: '#6BCB77'},
+    container: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 50 
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "white",
+        textAlign: "center"
+    },
+    switchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "80%", 
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold"
+    }
 });
