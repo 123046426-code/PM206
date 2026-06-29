7e3d48c89482f747f3231a5347c544bc28a81de0
Spica55 <123046426@upq.edu.mx>
2026-06-29 14:04:45 -0600

    Práctica 14 FlatList & SectionList

--- practica5/miapp2/screens/FlatListScreen.js
+++ practica5/miapp2/screens/FlatListScreen.js
@@ -1,15 +1,103 @@
 /*Zona1: Importaciones de componentes y archivos*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList, SectionList, Button } from 'react-native';
+import { useState } from 'react';
 
 
 //Zona2: Main - Hogar de los componentes
 export default function FlatListScreen () {
+  const [elementos, setElementos] = useState([
+    { id: '1', nombre: 'Elemento A' },
+    { id: '2', nombre: 'Elemento B' },
+    { id: '3', nombre: 'Elemento C' },
+    { id: '4', nombre: 'Elemento D' },
+    { id: '5', nombre: 'Elemento E' },
+    { id: '6', nombre: 'Elemento F' },
+    { id: '7', nombre: 'Elemento G' },
+    { id: '8', nombre: 'Elemento H' },
+    { id: '9', nombre: 'Elemento I' },
+    { id: '10', nombre: 'Elemento J' },
+    { id: '11', nombre: 'Elemento K' },
+    { id: '12', nombre: 'Elemento L' },
+    { id: '13', nombre: 'Elemento M' },
+    { id: '14', nombre: 'Elemento N' },
+    { id: '15', nombre: 'Elemento O' },
+  ]);
+
+  const [secciones, setSecciones] = useState([
+    {
+      tituloCategoria: 'Refrescos',
+      data: ['Cocacola', 'Fanta', 'Pepsi', 'Sprite'],
+    },
+    {
+      tituloCategoria: 'Papas',
+      data: ['Chips', 'Rufles', 'Takis', 'Sabritas'],
+    },
+    {
+      tituloCategoria: 'Jugos',
+      data: ['Jumex', 'Del Valle', 'FuzeTea', 'Arizona'],
+    },
+    {
+      tituloCategoria: 'Galletas',
+      data: ['Chokis', 'Principe', 'Emperador', 'Animalitos'],
+    },
+    {
+      tituloCategoria: 'Cacahuates',
+      data: ['Mafer', 'Japoneses', 'Garapiñado', 'Pistaches'],
+    },
+    {
+      tituloCategoria: 'Gomitas',
+      data: ['Tiburon', 'Fanta', 'Pepsi', 'Sprite'],
+    },
+  ]);
+
+  const eliminarElemento = (id) => {
+    setelementos(elementos.filter(item => item.id != id));
+  };
+
+  const renderContenidoSuperior = () => {
+    <View>
+      <Text style={styles.titulo}>Practica FlatList</Text>
+
+      <FlatList
+      data={elementos}
+      keyExtractor={(item) => item.id}
+      scrollEnabled={false}
+      renderItem={({item}) => (
+        <View style={styles.itemContainer}>
+          <Text style={styles.itemText}>{item.nombre}</Text>
+          <Button title="Eliminar" onPress={() => eliminarElemento(item.id)} />
+        </View>
+      )}
+      />
+
+      <View style={styles.barraDivisora}/>
+      <Text style={styles.titulo}>Practica SectionList</Text>
+    </View>
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Aqui va la práctica de Tung tung tung Saúl</Text>
+
+    
+    
+    <SectionList
+      sections={secciones}
+      keyExtractor={(item, index) => item + index}
+      ListHeaderComponent={renderContenidoSuperior}
+      renderItem={({item}) => (
+        <View style={styles.itemSection}>
+          <Text style={styles.itemText}>{item}</Text>
+        </View>
+      )}
+
+      renderSectionHeader={({section: { tituloCategoria }}) => (
+        <View style={styles.encabezado}>
+          <Text style={styles.textoEncabezado}>{tituloCategoria}</Text>
+        </View>
+      )}
+      />
       <StatusBar style="auto"/>
-      
     </View>
   );
 }
@@ -18,11 +106,46 @@ export default function FlatListScreen () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    paddingTop: 60,
+    paddingHorizontal: 20,
   },
-  tarjetaRoja:{backgroundColor: '#FF6B6B'},
-  tarjetaVerde:{backgroundColor: '#6BCB77'},
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  itemFlat: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  itemSection: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#b50000',
+  },
+  encabezado: {
+    backgroundColor: '#ff0202',
+    padding: 8,
+    marginTop: 15,
+  },
+  textoEncabezado: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  texto: {
+    fontSize: 16,
+  },
+  barraDivisora: {
+    height: 2,
+    backgroundColor: '#444444',
+    marginVertical: 30,
+    borderRadius: 1,
+  },
 });
