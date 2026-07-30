fa40b3ffe715142f3098f90451b8bbadcacc3774
Spica55 <123046426@upq.edu.mx>
2026-07-30 16:01:37 -0600

    practica 20

--- practica19/usuarioApi/usuarioApi/screens/AltaUsuariosScreen.js
+++ practica19/usuarioApi/usuarioApi/screens/AltaUsuariosScreen.js
@@ -1,9 +1,47 @@
 import React, { useState } from 'react';
-import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,} from 'react-native';
+import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet, Alert, Platform} from 'react-native';
 
 export default function App() {
   const [nombre, setNombre] = useState('');
   const [edad, setEdad] = useState('');
+  const [cargando, setCargando] = useState(false);
+
+  const mostrarMensaje = (titulo, mensaje)=>{
+    if(Platform.OS === 'web') {
+      window.alert(`${titulo}\n${mensaje}`);
+    }else{
+      Alert.alert(titulo, mensaje)
+    }
+  };
+
+  const guardarUsuario= async()=>{
+    if(nombre.trim()=== '' || edad.trim() === ''){
+      mostrarMensaje("Vacios", "Debes completar el formulario");
+      return;
+    }
+
+    try{
+      setCargando(true);
+      const respuesta= await fetch('http://localhost:5000/v1/usuarios', 
+        {
+          method: 'POST',
+          headers: {"content-type": "application/json"},
+          body: JSON.stringify({nombre:nombre, edad:Number(edad)})
+        }
+      );
+      const datos= await respuesta.json();
+      console.log("Respuesta API", datos);
+      mostrarMensaje("Exito", "Usuario agregado correctamente");
+      setNombre('');
+      setEdad('');
+    }catch(error){
+      console.log("Error al agregar usuario", error);
+      mostrarMensaje("Error", "No fue posible guardar");
+    }
+    finally{
+      setCargando(false);
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,9 +67,9 @@ export default function App() {
           onChangeText={setEdad}
         />
 
-        <Pressable style={styles.boton}>
+        <Pressable style={styles.boton}onPress={guardarUsuario} disabled={cargando}>
           <Text style={styles.textoBoton}>
-            Agregar Usuario
+            {cargando ? 'Guardando...' : 'Agregar Usuario'}
           </Text>
         </Pressable>
 
